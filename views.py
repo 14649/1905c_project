e14b702b237c46539760ba64eaf0995564e79980
14649 <1464953387@qq.com>
2020-07-17 11:52:13 +0800

    增加了注册功能

--- views.py
+++ views.py
@@ -1,8 +1,14 @@
 from django.http import HttpResponse
 from django.shortcuts import redirect
 
+
 def index(request):
        return HttpResponse("index")
 
+
 def login(request):
        return redirect("/index")
+
+
+def res(request):
+       return HttpResponse("res")
